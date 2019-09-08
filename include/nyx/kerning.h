724dbb136d45003d8823f0802fdb0eea45920d91
kiilas <kiilas@eluron.com>
2019-09-08 01:37:08 +0100

    fix arg naming consistency

--- include/nyx/kerning.h
+++ include/nyx/kerning.h
@@ -8,7 +8,7 @@ int nyx_font_set_kerning(int kerning);
 int nyx_font_kerning_num_pairs(size_t *pairs);
 int nyx_font_kerning_pair(uint32_t prev, uint32_t next);
 int nyx_font_kerning_pair_set(uint32_t prev, uint32_t next, int16_t offset);
-int nyx_font_kerning_pair_by_index(uint64_t index, NYX_KERNING_PAIR *pair);
+int nyx_font_kerning_pair_by_index(uint64_t idx, NYX_KERNING_PAIR *pair);
 
 int nyx_font_kerning_auto_pair(uint32_t prev, uint32_t next);
 int nyx_font_kerning_auto_range(uint32_t prev_min, uint32_t prev_max, uint32_t next_min, uint32_t next_max);
--- src/_glyph.h
+++ src/_glyph.h
@@ -25,11 +25,11 @@ uint32_t _glyphmap_size(const NYX_MAP *glyphs);
 struct glyph *_glyphmap_get(const NYX_MAP *glyphs, uint32_t code);
 struct glyph *_glyphmap_by_index(const NYX_MAP *glyphs, uint32_t idx);
 uint32_t _glyphmap_code_by_index(const NYX_MAP *glyphs, uint32_t idx);
-int _glyphmap_add_glyph(NYX_MAP *glyphs, uint32_t key, struct glyph *glyph);
-int _glyphmap_add(NYX_MAP *glyphs, uint32_t key, uint16_t w, void *bits);
+int _glyphmap_add_glyph(NYX_MAP *glyphs, uint32_t code, struct glyph *glyph);
+int _glyphmap_add(NYX_MAP *glyphs, uint32_t code, uint16_t w, void *bits);
 
-int _glyphmap_exists(const NYX_MAP *glyphs, uint32_t key);
-int _glyphmap_width(const NYX_MAP *glyphs, uint32_t key);
-const void *_glyphmap_bits(const NYX_MAP *glyphs, uint32_t key);
+int _glyphmap_exists(const NYX_MAP *glyphs, uint32_t code);
+int _glyphmap_width(const NYX_MAP *glyphs, uint32_t code);
+const void *_glyphmap_bits(const NYX_MAP *glyphs, uint32_t code);
 
 #endif
